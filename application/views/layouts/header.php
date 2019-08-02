91f19ec9ce7fda949dc84d4e7bf692f7cf7d155a
agungsenjaya <agungsenjaya813@gmail.com>
2019-08-02 14:50:22 +0700

    'satu'

---
+++ application/views/layouts/header.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Dashboard Penggajian</title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/theme.css">
+	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			
+		
