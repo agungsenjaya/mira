94149499cd63730acfa09213927c935865a4bb29
agungsenjaya <agungsenjaya813@gmail.com>
2019-08-08 14:49:54 +0700

    'bu'

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -5,8 +5,8 @@
 	<script src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap4.min.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
   <script src="<?php echo base_url(); ?>assets/js/number_only.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/select2.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/jquery.mask.min.js"></script>
   <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
 </body>
 </html>
--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -9,14 +9,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Dashboard Penggajian</title>
 	<link rel="icon" href="<?php echo base_url(''); ?>assets/favicon.png">
 	<link rel="stylesheet" href="">
+
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/theme.css">
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/style.css">
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/dataTables.bootstrap4.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/css/select2.css">
+	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/select2.css">
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/select.css">
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/fonts/FontAwesome.otf">
 	<link rel="stylesheet" href="<?php echo base_url(''); ?>assets/fonts/fontawesome-webfont.ttf">
+
 </head>
 <body>
 	<div class="container-fluid">
